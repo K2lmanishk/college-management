b7c4bdcf31774815eabc034a10539f56383cb220
k2lmanish <manish865133@gmail>
2026-09-19 17:32:25 +0530

    Add Render build script and pin Python

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate --no-input
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+  python manage.py createsuperuser --no-input || true
+fi
